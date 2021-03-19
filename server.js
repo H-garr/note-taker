168a8d320ae678882fd7608bbe79745088e1a47f
Hayden Garrard <hagarrard@gmail.com>
2021-03-19 09:48:55 -0500

    heroku work!

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+// added env for heroku deployment
+// takes in and recieves data and converts it from json.
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// middleware
+app.use(express.static("public"));
+
+// created calls here so it will grab and data that is 'wanted' 
+require("../routes/apiroutes")(app);
+require("../routes/htmlroutes")(app);
+// module export app for both of them (kept names same to be consistent)
+// where the server is being opened @
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
