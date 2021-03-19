d8893ffc92bedd64f93cadf5e711e274340eb836
Hayden Garrard <hagarrard@gmail.com>
2021-03-19 09:50:33 -0500

    heroku

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // created calls here so it will grab and data that is 'wanted' 
-require("../routes/apiroutes")(app);
-require("../routes/htmlroutes")(app);
+require("./routes/apiroutes")(app);
+require("./routes/htmlroutes")(app);
 // module export app for both of them (kept names same to be consistent)
 // where the server is being opened @
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
